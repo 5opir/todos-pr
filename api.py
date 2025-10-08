c870ecc0638a44541eb5d46712a47c2d613e3772
Исламов Руслан ИВТ-432(б) <catgamer@inbox.ru>
2025-10-08 22:11:51 +0300

    pr-4 маршрутизация fast-api

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from todo import todo_router
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return { "message": "Hello again! Hello Ruslan Islamov!"}
+
+app.include_router(todo_router)
