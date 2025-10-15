9db5fefdf30d96b8bdfd43e27fa6632f5931ca80
Исламов Руслан ИВТ-432(б) <catgamer@inbox.ru>
2025-10-15 22:07:11 +0300

    pr-5 создание crud-приложения, модели ответов и обработка ошибок

--- model.py
+++ model.py
@@ -1,13 +1,33 @@
 from pydantic import BaseModel
+from typing import List
 
 class Todo(BaseModel):
     id: int
     item: str
 
+class TodoItem(BaseModel):
+    item: str
+
     class Config:
-        Schema_extra = {
-            "Example": {
-                "id": 1,
-                "item": "Example schema!"
+        schema_extra = {
+            "example": {
+                "item": "Ruslan Islamov, you should read the next chapter of the book!"
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1"
+                    },
+                    {
+                        "item": "Example schema 2"
+                    }
+                ]
             }
         }
--- todo.py
+++ todo.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter, Path, Query
-from model import Todo
+from fastapi import APIRouter, Path, Query, HTTPException, status
+from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
-@todo_router.post("/todo")
+@todo_router.post("/todo", status_code=201)
 async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {"message": "Todo добавлен успешно!"}
 
-@todo_router.get("/todo")
+@todo_router.get("/todo", response_model=TodoItems)
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
 
@@ -21,10 +21,36 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
             return {
                 "todo": todo
             }
-    return {
-        "message": "Todo with supplied ID doesn't exist."
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Задача с указанным ID не найдена."
+    )
 
 @todo_router.get("/search/")
 async def query_route(query: str = Query(None)):
     return {"query": query}
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="ID задачи")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {
+                "message": "Задача обновлена."
+            }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Задача с указанным ID не найдена."
+    )
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {"message": "Задача удалена."}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Задача с указанным ID не найдена."
+    )
